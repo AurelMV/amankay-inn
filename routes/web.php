ea248597b18d4e17653172edba0c1a247e002093
miguel <josdotaperalta225@gmail.com>
2025-05-26 10:48:42 +0200

    interfaces

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/login', function () {
+    return view('iniciosesion');
+});
+Route::get('/usuario', function () {
+    return view('usuario');
+});
+Route::get('/catalogo', function () {
+    return view('catalogo');
+});
