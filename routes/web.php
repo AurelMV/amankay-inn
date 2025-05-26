d769d9c2976bdc1f18da72b1397f69084be20e05
Edy Joseph Cáceres Montes <edyleocaceres@gmail.com>
2025-05-26 09:17:21 -0500

    vistas

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,9 @@ use App\Http\Controllers\SocialController;
 Route::get('/', function () {
     return view('welcome');
 });
-
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
 
 Route::get('/auth/{provider}', [SocialController::class, 'redirectToProvider']);
 Route::get('/auth/{provider}/callback', [SocialController::class, 'handleProviderCallback']);
